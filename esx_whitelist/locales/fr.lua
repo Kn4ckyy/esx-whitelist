8d076646141128140ddd8d33f4f05b0226a568b8
Kn4ckyy <52577120+Kn4ckyy@users.noreply.github.com>
2019-07-05 17:50:54 +0200

    Add files via upload

---
+++ esx_whitelist/locales/fr.lua
@@ -0,0 +1,8 @@
+Locales['fr'] = {
+  ['whitelist_check']     = 'Nous verifions si vous ête whitelist.',
+  ['not_whitelisted']     = 'ceci est un serveur avec whitelist',
+  ['steamid_error']       = 'nous n\'arrivons pas à lire votre SteamID',
+  ['whitelist_empty']     = 'Vous ne faites pas partie de la whitelist.',
+  ['help_whitelist_add']  = 'ajouter quelqu\'un dans la Whitelist',
+  ['help_whitelist_load'] = 'recharger la Whitelist',
+}
